81189c300d1ace69c62eda0dd20635dc161df9d1
eisendaniel <danieleisen99@gmail.com>
2021-06-06 12:18:15 +1200

    skip invalid filepaths in input dir

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Batch Image Sequence Convertor in Rust
 
+Uses image + rayon to make a quick and easy batch image sequence reencoder. Every one I found was slow and I didn't need a UI, so this.
+
 ## Usage
 
 Pass path of directory containing image sequence (of .tiffs for example) and extension to convert to.
@@ -7,5 +9,9 @@ Pass path of directory containing image sequence (of .tiffs for example) and ext
 `./im_seq_cvtr ~/Pictures/input_img png`
 
 Note: exclude trailing `/` to place in dir next to original, include to place inside original dir.
+
+## Results
+
+Can recode ~1000 tiff to png in ~30sec 
 
 
--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,15 @@ use std::env;
 use std::path::{Path, PathBuf};
 
 fn main() {
-    let (from, extension) = if env::args_os().count() == 3 {
-        (
-            env::args_os().nth(1).unwrap(),
-            env::args_os().nth(2).unwrap(),
-        )
+    let (from, extension) = if env::args().count() == 3 {
+        (env::args().nth(1).unwrap(), env::args().nth(2).unwrap())
     } else {
-        println!("Error: Missing Args!\nUsage: ./img_seq_cvtr [folderpath] [extension]\ne.g. ./img_seq_cvtr ~/tiff_seq png");
+        eprintln!("Error: Missing Args!\nUsage: ./img_seq_cvtr [folderpath] [extension]\ne.g. ./img_seq_cvtr ~/tiff_seq png");
         std::process::exit(1);
     };
 
     if !Path::new(&from).is_dir() {
-        println!("Error: given path is invalid");
+        eprintln!("Error: given input path is invalid");
         std::process::exit(1);
     }
 
@@ -23,25 +20,24 @@ fn main() {
         .unwrap()
         .map(|x| x.unwrap().path())
         .collect();
-    //add output path
-    let output_dir = format!(
-        "{}_converted_to_{}",
-        from.to_str().unwrap(),
-        extension.to_str().unwrap()
-    );
+    //construct output dir string
+    let output_dir = format!("{}_converted_to_{}", &from, &extension);
     match std::fs::create_dir(&output_dir) {
         Ok(_) => {
+            let now = std::time::Instant::now();
             paths.into_par_iter().for_each(|in_path| {
-                let mut out_path = PathBuf::from(&output_dir);
-                out_path.push(in_path.file_stem().unwrap().to_str().unwrap());
-                out_path.set_extension(extension.to_str().unwrap());
+                let mut out_path = PathBuf::from(&output_dir); //parent path
+                out_path.push(in_path.file_stem().unwrap().to_str().unwrap()); //read and append filename to parent
+                out_path.set_extension(&extension);
 
-                let tiff = image::open(&in_path).unwrap();
-                tiff.save(out_path).unwrap();
+                if let Ok(tiff) = image::open(&in_path) {
+                    tiff.save(out_path).unwrap();
+                }
             });
+            println!("{}", now.elapsed().as_millis());
         }
         Err(e) => {
-            println!("{:?}", e);
+            eprintln!("Unable to create output directory\nError: {:?}", e);
         }
     }
 }
